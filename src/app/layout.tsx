7f753023879a960eb3cc1023d5e0d2809961f62d
Geetika Das <dgeeikadoll@gmail.com>
2025-04-28 01:49:45 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import RootLayout from "@/components/layout/root-layout";
 export const metadata: Metadata = {
   title: "BrewDistrict24 | Classic craft beers, brewed without fuss",
   description: "Welcome to the neighbourhood. Our classic craft beers are brewed without fuss. Pure and honest. Damn delicious.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function Layout({
